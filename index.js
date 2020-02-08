d5804e812d8f21f2663b9826e1232591e0cc805b
unknown <thashmi959@gmail.com>
2020-02-09 04:02:16 +0530

    moving private information

--- index.js
+++ index.js
@@ -2,17 +2,31 @@ const express = require("express");
 
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const config = require("./config/key");
+
+const { User } = require("./models/user");
 
 mongoose
-	.connect(
-		"mongodb+srv://thashmi959:Htarique@123@cluster0-i4plb.mongodb.net/react-blog?retryWrites=true&w=majority",
-		{ useNewUrlParser: true, useUnifiedTopology: true }
-	)
+	.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() => console.log("MongoDb Connected..."))
 	.catch(err => console.log(err));
 
-app.get("/", (req, res) => {
-	res.send("Hello from Express...");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.post("/api/users/register", (req, res) => {
+	const user = new User(req.body);
+
+	user.save((err, userData) => {
+		if (err) return res.json({ success: false, err });
+		return res.status(200).json({
+			success: true
+		});
+	});
 });
 
 app.listen(5000);
--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-	name: {
+	firstname: {
 		type: String,
 		maxlength: 50
 	},
